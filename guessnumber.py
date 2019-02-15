2f19aace91a35b4f867d7cd21b001dffac08d2bc
MauriceLee <ee.maurice.lee@gmail.com>
2019-02-16 03:37:51 +0800

    add counter

--- guessnumber.py
+++ guessnumber.py
@@ -1,11 +1,14 @@
 import random
 
 r = str(random.randint(1, 100))
+c = 0
 
 while True:
     g = input('Please guess a number: ')
+    c = c + 1
     if g == r:
         print('Finally right !')
+        print('You guess the ', c, ' times.')
         break
     elif g > r:
         print('Upper than the answer.')
